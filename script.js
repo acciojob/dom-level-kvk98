ca74281f9e80fd62327dbdc720b695ea62e5b02a
kvk98 <119598315+kvk98@users.noreply.github.com>
2023-11-07 13:59:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 //your JS code here. If required.
-const ele = document.getElementId("level");
+const ele = document.getElementById("level");
+
 let count = 0;
 while (ele.parentNode != html) {
 	ele = ele.parentNode;
