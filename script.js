0ca57de02b81c03c9e9a7e01b27b542c08806d46
kvk98 <119598315+kvk98@users.noreply.github.com>
2023-11-07 13:56:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+const ele = document.getElementId("level");
+let count = 0;
+while (ele.parentNode != html) {
+	ele = ele.parentNode;
+	count++;
+}
+alert("The level of the element is: " + count);
